c61febd0901f97955210bb2d7a28c5a6c96d7029
Izuku-mi <151708571+izuku-mii@users.noreply.github.com>
2025-08-14 16:04:51 +0700

    Create id-info.js

---
+++ plugins/id-info.js
@@ -0,0 +1,11 @@
+const handler = async (m, { conn, text }) => {
+  const userId = m.sender;
+  await conn.sendMessage(m.chat, { text: userId }, { quoted: { message_id: m.id } })
+}
+
+handler.command = /^(id)$/i
+handler.help = ['id', 'getid']
+handler.tags = ['info']
+handler.private = false
+
+export default handler
